790326a121fe29cce79d5c8f416e715ead5a2bae
AgentBlackout <joshruler3558@gmail.com>
2020-01-27 22:57:43 +0000

    Push code initial

---
+++ RainbowTagController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atlas.Settings.Models.Versioning;
+using UnityEngine;
+
+namespace ARainbowTags
+{
+    public class RainbowTagController : MonoBehaviour
+    {
+        private ServerRoles _roles;
+        private string _originalColor;
+        private const float _interval = .2f;
+
+        private string[] _possibleColors =
+        {
+            "pink",
+            "red",
+            "brown",
+            "silver",
+            "light_green",
+            "crimson",
+            "cyan",
+            "aqua",
+            "deep_pink",
+            "tomato",
+            "yellow",
+            "magenta",
+            "blue_green",
+            "orange",
+            "lime",
+            "green",
+            "emerald",
+            "carmine",
+            "nickel",
+            "mint",
+            "army_green",
+            "pumpkin"
+        };
+
+        private int _position = 0;
+        private float _nextCycle = 0f;
+
+        private void Start()
+        {
+            _nextCycle = Time.time;
+            _originalColor = _roles.NetworkMyColor;
+        }
+
+        private void OnEnable()
+        {
+            _roles = GetComponent<ServerRoles>();
+        }
+
+        private void OnDisable()
+        {
+            _roles.NetworkMyColor = _originalColor;
+        }
+
+        private void Update()
+        {
+            if (Time.time < _nextCycle) return;
+            _nextCycle += _interval;
+
+            _roles.NetworkMyColor = _possibleColors[_position];
+
+            if (++_position >= _possibleColors.Length)
+                _position = 0;
+
+        }
+    }
+}
---
+++ RainbowTagMod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atlas;
+using Object = UnityEngine.Object;
+
+namespace ARainbowTags
+{
+    [MetadataAttribute(
+        "Rainbow Tags",
+        "Adds rainbow scrolling colours to tags",
+        "AgentBlackout", "Androxanik"
+    )]
+
+    public class RainbowTagMod : Mod<RainbowSettings>
+    {
+        public RainbowTagMod(ModLoadInfo data) : base(data)
+        {
+            
+        }
+
+        private RainbowTagController AddRainbowController(ReferenceHub references, RainbowSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnPlayerAwake(ReferenceHub player)
+        {
+            Debug("Loaded init");
+            player.gameObject.AddComponent<RainbowTagController>();
+        }
+
+        protected override void Awake()
+        {
+            foreach(var player in PlayerManager.players)
+            {
+                player.gameObject.AddComponent<RainbowTagController>();
+            }
+        }
+
+        protected override void Destroy()
+        {
+            foreach (var player in PlayerManager.players)
+            {
+                Object.Destroy(player.GetComponent<RainbowTagController>());
+            }
+        }
+    }
+}
