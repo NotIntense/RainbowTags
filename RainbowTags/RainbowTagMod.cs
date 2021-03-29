21d9be424e45bff2f87b4905235551a27c896e0e
Build <jwbradley2003@gmail.com>
2021-03-29 09:58:16 -0400

    Version Bump

--- RainbowTags/RainbowTagMod.cs
+++ RainbowTags/RainbowTagMod.cs
@@ -19,7 +19,7 @@
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 9, 4);
 
-        public override Version Version { get; } = new Version(2, 2);
+        public override Version Version { get; } = new Version(2, 3, 0);
 
         public override void OnEnabled()
         {
