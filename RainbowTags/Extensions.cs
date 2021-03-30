d94965323d49652b7f3d8cf232e686b6056fd2c1
Build <jwbradley2003@gmail.com>
2021-03-30 11:50:54 -0400

    Group Null Check
    Prevents an error from occurring when the users group is null.

--- RainbowTags/Extensions.cs
+++ RainbowTags/Extensions.cs
@@ -8,6 +8,9 @@ namespace RainbowTags
         public static bool IsRainbowTagUser(this Player ply, out List<string> sequence)
         {
             sequence = null;
+            if (ply.Group == null)
+                return false;
+            
             string group = ply.Group.BadgeText;
             return !string.IsNullOrEmpty(group) && RainbowTagMod.Instance.Config.Sequences.TryGetValue(group, out sequence);
         }
