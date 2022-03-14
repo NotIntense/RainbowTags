2f283a4404883d0812191f978f8c5b226789a281
Build <jwbradley2003@gmail.com>
2022-03-14 19:33:17 -0400

    Add lower bound to tag interval config

--- RainbowTags/Config.cs
+++ RainbowTags/Config.cs
@@ -8,22 +8,37 @@
 namespace RainbowTags
 {
     using System.Collections.Generic;
+    using System.ComponentModel;
     using Exiled.API.Interfaces;
 
     /// <inheritdoc />
     public sealed class Config : IConfig
     {
+        private float tagInterval = 0.5f;
+
         /// <inheritdoc/>
         public bool IsEnabled { get; set; } = true;
 
         /// <summary>
         /// Gets or sets the time, in seconds, between switching to the next color in a sequence.
         /// </summary>
-        public float TagInterval { get; set; } = 0.5f;
+        [Description("The time, in seconds, between switching to the next color in a sequence.")]
+        public float TagInterval
+        {
+            get => tagInterval;
+            set
+            {
+                if (value < 0.5f)
+                    value = 0.5f;
+
+                tagInterval = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets a collection of group names with their respective color sequences.
         /// </summary>
+        [Description("A collection of group names with their respective color sequences.")]
         public Dictionary<string, string[]> Sequences { get; set; } = new Dictionary<string, string[]>
         {
             ["owner"] = new[]
