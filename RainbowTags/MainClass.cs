7191e20d20b3fac4beb2244b3f4569219f829ae1
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-04-17 00:27:09 +0200

    Minor Code Optimization and Update to Beta 3

--- RainbowTags/MainClass.cs
+++ RainbowTags/MainClass.cs
@@ -4,68 +4,64 @@ using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
 using Object = UnityEngine.Object;
 
-namespace RainbowTags
+namespace RainbowTags;
+
+public class MainClass : Plugin<Config>
 {
-    public class MainClass : Plugin<Config>
+    public override string Author => "xNexusACS (Ported From Build)";
+    public override string Name => "RainbowTags";
+    public override string Prefix => "RainbowTags";
+    public override Version Version { get; } = new(4, 0, 2);
+    public override Version RequiredExiledVersion { get; } = new(6, 0, 0);
+
+    public override void OnEnabled()
     {
-        public override string Author { get; } = "xNexusACS (Ported From Build)";
-        public override string Name { get; } = "RainbowTags";
-        public override string Prefix { get; } = "RainbowTags";
-        public override Version Version { get; } = new(4, 0, 1);
-        public override Version RequiredExiledVersion { get; } = new(6, 0, 0);
+        Exiled.Events.Handlers.Player.ChangingGroup += OnChangingGroup;
+        base.OnEnabled();
+    }
 
-        public override void OnEnabled()
-        {
-            Exiled.Events.Handlers.Player.ChangingGroup += OnChangingGroup;
-            base.OnEnabled();
-        }
+    public override void OnDisabled()
+    {
+        Exiled.Events.Handlers.Player.ChangingGroup -= OnChangingGroup;
+        base.OnDisabled();
+    }
 
-        public override void OnDisabled()
-        {
-            Exiled.Events.Handlers.Player.ChangingGroup -= OnChangingGroup;
-            base.OnDisabled();
-        }
+    private bool TryGetColors(string rank, out string[] availableColors)
+    {
+        availableColors = Config.Sequences;
+        return !string.IsNullOrEmpty(rank) && Config.RanksWithRTags.Contains(rank);
+    }
+    private bool EqualsTo(UserGroup thisGroup, UserGroup otherGroup)
+    {
+        return thisGroup.BadgeColor == otherGroup.BadgeColor && thisGroup.BadgeText == otherGroup.BadgeText &&
+               thisGroup.Permissions == otherGroup.Permissions && thisGroup.Cover == otherGroup.Cover &&
+               thisGroup.HiddenByDefault == otherGroup.HiddenByDefault && thisGroup.Shared == otherGroup.Shared &&
+               thisGroup.KickPower == otherGroup.KickPower &&
+               thisGroup.RequiredKickPower == otherGroup.RequiredKickPower;
+    }
+    private string GetGroupKey(UserGroup group)
+    {
+        if (group == null)
+            return string.Empty;
 
-        private bool TryGetColors(string rank, out string[] availableColors)
+        return ServerStatic.PermissionsHandler._groups.FirstOrDefault(g => EqualsTo(g.Value, group)).Key ??
+               string.Empty;
+    }
+    private void OnChangingGroup(ChangingGroupEventArgs ev)
+    {
+        if (ev.NewGroup != null && ev.Player.Group == null && TryGetColors(GetGroupKey(ev.NewGroup), out var colors))
         {
-            availableColors = Config.Sequences;
-            return !string.IsNullOrEmpty(rank) && Config.RanksWithRTags.Contains(rank);
+            Log.Debug("RTag: " + ev.Player.Nickname);
+            var rtController = ev.Player.GameObject.AddComponent<TagController>();
+            rtController.Colors = colors;
+            rtController.Interval = Config.ColorInterval;
+            return;
         }
-        private bool EqualsTo(UserGroup thisGroup, UserGroup otherGroup)
+        if (TryGetColors(GetGroupKey(ev.NewGroup), out colors))
         {
-            return thisGroup.BadgeColor == otherGroup.BadgeColor && thisGroup.BadgeText == otherGroup.BadgeText &&
-                   thisGroup.Permissions == otherGroup.Permissions && thisGroup.Cover == otherGroup.Cover &&
-                   thisGroup.HiddenByDefault == otherGroup.HiddenByDefault && thisGroup.Shared == otherGroup.Shared &&
-                   thisGroup.KickPower == otherGroup.KickPower &&
-                   thisGroup.RequiredKickPower == otherGroup.RequiredKickPower;
+            ev.Player.GameObject.GetComponent<TagController>().Colors = colors;
+            return;
         }
-        private string GetGroupKey(UserGroup group)
-        {
-            if (group == null)
-            {
-                return string.Empty;
-            }
-
-            return ServerStatic.PermissionsHandler._groups.FirstOrDefault(g => EqualsTo(g.Value, group)).Key ??
-                   string.Empty;
-        }
-        private void OnChangingGroup(ChangingGroupEventArgs ev)
-        {
-            string[] colors;
-            if (ev.NewGroup != null && ev.Player.Group == null && TryGetColors(GetGroupKey(ev.NewGroup), out colors))
-            {
-                Log.Debug("RTag: " + ev.Player.Nickname);
-                TagController rtController = ev.Player.GameObject.AddComponent<TagController>();
-                rtController.Colors = colors;
-                rtController.Interval = Config.ColorInterval;
-                return;
-            }
-            if (TryGetColors(GetGroupKey(ev.NewGroup), out colors))
-            {
-                ev.Player.GameObject.GetComponent<TagController>().Colors = colors;
-                return;
-            }
-            Object.Destroy(ev.Player.GameObject.GetComponent<TagController>());
-        }
+        Object.Destroy(ev.Player.GameObject.GetComponent<TagController>());
     }
 }
