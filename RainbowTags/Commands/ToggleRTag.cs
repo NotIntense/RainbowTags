2d7d15180221ccc4d54f2f54106a4e6efa1778fa
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-04-17 03:58:17 +0200

    command

---
+++ RainbowTags/Commands/ToggleRTag.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CommandSystem;
+using Exiled.API.Features;
+using RemoteAdmin;
+
+namespace RainbowTags.Commands;
+
+[CommandHandler(typeof(ClientCommandHandler))]
+[CommandHandler(typeof(RemoteAdminCommandHandler))]
+public class ToggleRTag : ICommand
+{
+    public string Command { get; } = "togglerainbowtag";
+    public string[] Aliases { get; } = { "trt" };
+    public string Description { get; } = "Toggles your rainbow tag on or off";
+
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+    {
+        if (sender is PlayerCommandSender playerCommandSender)
+        {
+            var player = Player.Get(playerCommandSender);
+            if (player == null)
+            {
+                response = "You must be in-game to use this command!";
+                return false;
+            }
+
+            if (player.GameObject.TryGetComponent(out TagController rainbowTag))
+            {
+                player.RemoveComponent(rainbowTag);
+                response = "Your rainbow tag has been disabled!";
+                return true;
+            }
+
+            player.GameObject.AddComponent<TagController>();
+            response = "Your rainbow tag has been enabled!";
+            return true;
+        }
+
+        response = "You must be in-game to use this command!";
+        return false;
+    }
+}
---
+++ RainbowTags/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.API.Features;
+using UnityEngine;
+
+namespace RainbowTags;
+
+public static class Extensions
+{
+    public static void RemoveComponent(this Player player, Component component)
+    {
+        if (player.GameObject.GetComponent(component.GetType()) != null)
+            Object.Destroy(component);
+    }
+    
+    public static void RemoveComponent<T>(this Player player) where T : Component
+    {
+        var component = player.GameObject.GetComponent<T>();
+        if (component != null)
+            Object.Destroy(component);
+    }
+}
